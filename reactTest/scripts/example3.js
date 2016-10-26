4dd0332bf896faf38f3e50241b638fa48c205b02
eduardo.azuaga@autologica.com <eduardo.azuaga@autologica.com>
2016-10-25 23:08:59 -0200

    first commit

---
+++ reactTest/scripts/example3.js
@@ -0,0 +1,19 @@
+﻿
+var Test = React.createClass({
+    render: function() {
+        var stationComponents = this.props.stations.map(function(station) {
+            return <div className="station">{station.call}</div>;
+        });
+        return <div>{stationComponents}</div>;
+    }
+});
+
+
+
+var stations = [
+  {call:'station one',frequency:'000'},
+  {call:'station two',frequency:'001'}
+]; 
+
+ReactDOM.render(<Test stations={stations} />, document.getElementById('container'));
+ReactDOM.render(<Test stations={stations} />, document.getElementById('content'));
---
+++ reactTest/scripts/galery.js
@@ -0,0 +1,22 @@
+﻿var Galery = React.createClass({
+    ClickImg :function(){
+        console.log(this);
+        alert("asdsa");
+    },
+    render: function () {
+        var article = this.props.articles.map(function(i){
+            return <article  ><h1>{i.title}</h1><img onClick={this.ClickImg} src={i.src} /><div>{i.description}</div></article> ;
+        },this);
+        return <div>{article}</div>;
+    }
+
+})
+
+var articles = [
+  {title:'art1',src:'facebook.jpg' , description:'articulo1'},
+    {title:'art2',src:'twitter.jpg' , description:'articulo2'},
+      {title:'art3',src:'000' , description:'articulo3'}
+
+]; 
+
+ReactDOM.render(<Galery articles={articles} />, document.getElementById('container'));
